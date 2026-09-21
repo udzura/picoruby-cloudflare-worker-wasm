664c67d994928b195b5eda4a48a5224f9baedf8a
Uchio Kondo <udzura@udzura.jp>
2026-09-21 21:22:14 +0900

    Support reading from streams in the Ruby context

--- spike/test/queue_app.rb
+++ spike/test/queue_app.rb
@@ -29,6 +29,29 @@ class QueueTestConsumer < Cloudflare::QueueConsumer::Base
         current_batch.ack_all
       when "retry-all"
         current_batch.retry_all(delay_seconds: 30)
+      when "r2-read-all"
+        object = bindings.BUCKET.get("queue.txt")
+        raise "Queue R2 object is missing" unless object
+        raise "Queue R2 body did not match" unless object.body.read_all == "queue body"
+        message.ack
+      when "r2-read-partial"
+        object = bindings.BUCKET.get("queue.txt")
+        raise "Queue R2 object is missing" unless object
+        stream = object.body
+        raise "Queue R2 partial body did not match" unless stream.read_partial(3) == "que"
+        raise "Queue R2 remaining body did not match" unless stream.read_all(max_bytes: 7) == "ue body"
+        raise "Queue R2 stream did not reach EOF" unless stream.read_partial(1).nil?
+        message.ack
+      when "r2-read-too-large"
+        object = bindings.BUCKET.get("queue.txt")
+        raise "Queue R2 object is missing" unless object
+        begin
+          object.body.read_all(max_bytes: 3)
+        rescue ArgumentError
+          message.ack
+        else
+          raise "Queue R2 body exceeded max_bytes without an error"
+        end
       end
     end
     [:info, { "queue" => current_batch.queue, "count" => current_batch.messages.size }, "Queue batch processed"]
