1c0ee2532925f3244da1298789b545cca81a2f5d
Uchio Kondo <udzura@udzura.jp>
2026-09-22 11:55:03 +0900

    Vendor mruby-time for forking

---
+++ vendor/mruby-time/include/mruby/time.h
@@ -0,0 +1,27 @@
+/*
+** mruby/time.h - Time class
+**
+** See Copyright Notice in mruby.h
+*/
+
+#ifndef MRUBY_TIME_H
+#define MRUBY_TIME_H
+
+#include <mruby/common.h>
+#include <time.h>
+
+MRB_BEGIN_DECL
+
+typedef enum mrb_timezone {
+  MRB_TIMEZONE_NONE   = 0,
+  MRB_TIMEZONE_UTC    = 1,
+  MRB_TIMEZONE_LOCAL  = 2,
+  MRB_TIMEZONE_LAST   = 3
+} mrb_timezone;
+
+MRB_API mrb_value mrb_time_at(mrb_state *mrb, time_t sec, time_t usec, mrb_timezone timezone);
+MRB_API struct tm* mrb_time_get_tm(mrb_state *mrb, mrb_value time);
+
+MRB_END_DECL
+
+#endif /* MRUBY_TIME_H */
